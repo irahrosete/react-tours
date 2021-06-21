a90bd395f4a11b608a17a15e3165a3f38327ce0c
irah rosete <irah.rosete@outlook.com>
2021-06-22 01:16:06 +1000

    add loading state and read more button

--- src/App.js
+++ src/App.js
@@ -4,42 +4,57 @@ import './App.css'
 const url = 'https://course-api.com/react-tours-project'
 const App = () => {
   const [tours, setTours] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
     fetch(url)
       .then((response) => response.json())
       .then((data) => {
+        setIsLoading(false)
         setTours(data)
-        // console.log(data)
       })
       .catch((error) => console.log(error))
   }, [])
 
   const removeTour = (id) => {
-    console.log(tours)
     let newTours = tours.filter((tour) => tour.id !== id)
     setTours(newTours)
   }
 
-  return (
-    <>
-      <h1>Our Tours</h1>
-      {tours.map((tour) => {
-        const { id, name, info, price, image } = tour
-        return (
-          <div key={id} className='container'>
-            <img src={image} alt={name} className='img' />
-            <div>
-              <h3>{name}</h3>
-              <p>{price}</p>
-              <p>{info}</p>
-              <button onClick={() => removeTour(id)}>Not Interested</button>
-            </div>
-          </div>
-        )
-      })}
-    </>
-  )
+  const refreshTours = () => {
+    console.log(tours)
+  }
+
+  if (isLoading) {
+    return <h1>Loading...</h1>
+  } else {
+    return (
+      <>
+        <h1>Our Tours</h1>
+        {tours.length < 1 ? (
+          <button onClick={refreshTours}>Refresh</button>
+        ) : (
+          tours.map((tour) => {
+            const { id, name, info, price, image } = tour
+            return (
+              <div key={id} className='container'>
+                <img src={image} alt={name} className='img' />
+                <div>
+                  <h3>{name}</h3>
+                  <p>$ {price}</p>
+                  <p>
+                    {info}
+                    <button>Read More</button>
+                  </p>
+                  <button onClick={() => removeTour(id)}>Not Interested</button>
+                </div>
+              </div>
+            )
+          })
+        )}
+      </>
+    )
+  }
 }
 
 export default App
